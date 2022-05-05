c9abcc752bd74b8eb588d6ddc9dd295728589a66
DevCeur <dev.carlos.umana@gmail.com>
2022-05-05 00:34:38 -0500

    [feat]: create createMeta utility

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,13 @@
-export default function HomeRoute() {
+import { createMeta } from "~/utils/createMeta";
+
+export const meta = createMeta({ title: "Home" });
+
+const HomeRoute = () => {
   return (
     <div>
       <h1 className="text-red-500">DiEvent</h1>
     </div>
   );
-}
+};
+
+export default HomeRoute;
